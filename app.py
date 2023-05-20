1408d2d6fb02a1597dbe039e75cc33ca3472cd5d
allem40306 <allem40306@gmail.com>
2023-05-20 16:08:52 +0000

    新增功能：連線到資料庫列出 table

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
+import configparser
 
-db = SQLAlchemy()
+config = configparser.ConfigParser()
+config.read_file(open("dev.ini"))
+username = config.get('DATABASE', 'username')
+password = config.get('DATABASE', 'password')
+host = config.get('DATABASE', 'host')
+port = config.get('DATABASE', 'port')
+database = config.get('DATABASE', 'database')
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{username}:{password}@{host}:{port}/{database}"
+db = SQLAlchemy(app)
 
 @app.route("/")
 def hello_world():
@@ -17,3 +28,12 @@ def json():
 def json_with_id(test_id):
     data = {"id": test_id}
     return jsonify(data)
+
+
+@app.route('/table/')
+def show_table():
+    sql_cmd = text("show tables")
+    result = db.session.execute(sql_cmd)
+    data = [row[0] for row in result]
+    print(data)
+    return data
