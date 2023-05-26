cce5100bc579a4352920e3116575d5c377d5b73f
allem40306 <allem40306@gmail.com>
2023-05-26 08:02:43 +0000

    新增：ORM 修改：table 資訊

--- app.py
+++ app.py
@@ -10,22 +10,39 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 class Teacher(db.Model):
-    __tablename__ = 'teacher'
-    name = db.Column(db.VARCHAR(30), primary_key = True)
-    email = db.Column(db.VARCHAR(30))
-    office = db.Column(db.VARCHAR(30))
+    __tablename__ = "teacher"
+    name = db.Column(db.VARCHAR(30), primary_key = True, nullable = False)
+    email = db.Column(db.VARCHAR(30), nullable = False)
+    office = db.Column(db.VARCHAR(30), nullable = False)
+    course = db.relationship("Course", backref = "teacherInfo")
 
-class ClassRoom(db.Model):
-    __tablename__ ='classroom'
-    id = db.Column(db.INT, primary_key = True)
-    floor = db.Column(db.INT)
-    capacity = db.Column(db.INT)
+    def __init__(self, name, email, office):
+        self.name = name
+        self.email = email
+        self.office = office
+
+class Classroom(db.Model):
+    __tablename__ = "classroom"
+    id = db.Column(db.VARCHAR(30), primary_key = True, nullable = False)
+    floor = db.Column(db.INT, nullable = False)
+    capacity = db.Column(db.INT, nullable = False)
+    course = db.relationship("Course", backref = "classroomInfo")
+
+    def __init__(self, id, floor, capacity):
+        self.id = id
+        self.floor = floor
+        self.capacity = capacity
 
 class Course(db.Model):
-    __tablename__ = 'course'
+    __tablename__ = "course"
     name = db.Column(db.VARCHAR(30), primary_key = True)
-    teacher = db.Column(db.VARCHAR(30), db.ForeignKey("teacher.name"))
-    classRoom = db.Column(db.INT, db.ForeignKey("classroom.id"))
+    teacher = db.Column(db.VARCHAR(30), db.ForeignKey("teacher.name"), nullable = False)
+    roomCode = db.Column(db.VARCHAR(30), db.ForeignKey("classroom.id"), nullable = False)
+
+    def __init__(self, name, teacher, roomCode):
+        self.name = name
+        self.teacher = teacher
+        self.roomCode = roomCode
 
 @app.route("/")
 def hello_world():
@@ -46,5 +63,52 @@ def show_table():
     sql_cmd = text("show tables")
     result = db.session.execute(sql_cmd)
     data = [row[0] for row in result]
-    print(data)
     return data
+
+@app.route('/create')
+def create():
+    classroom1 = Classroom("CD321", 5, 20)
+    classroom2 = Classroom("CD322", 4, 22)
+    teacher1 = Teacher(name = "Sam", email = "sam@test.com", office = "AB123")
+    teacher2 = Teacher(name = "Ann", email = "ann@test.com", office = "AB124")
+    course1 = Course("Data Structure", "Sam", "CD321")
+    course2 = Course("Algorithm", "Ann", "CD322")
+
+    db.session.add(classroom1)
+
+    data = [classroom2, teacher1, teacher2]
+    db.session.add_all(data)
+
+    db.session.add_all([course1, course2])
+    db.session.commit()
+    
+    return "OK Success"
+
+@app.route('/select')
+def select():
+    print(Course.__tablename__)
+    res1 = Course.query.filter_by(name = "Algorithm").first()
+    print(f"Course: {res1.name, res1.teacher, res1.roomCode}")
+    print(f"teacher: {res1.teacherInfo.email, res1.teacherInfo.office}")
+    
+    res2 = Teacher.query.all()
+    for res in res2:
+        print(res.name, res.email, res.office)
+
+    return "OK Success"
+
+@app.route('/update')
+def update():
+    query = Course.query.filter_by(name = "Algorithm").first()
+    query.roomCode = "CD321"
+    db.session.commit()
+
+    return "OK Success"
+
+@app.route('/delete')
+def delete():
+    query = Course.query.filter_by(name = "Algorithm").first()
+    db.session.delete(query)
+    db.session.commit()
+
+    return "OK Success"
