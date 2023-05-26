96292cf55c85a9ef9643e89632c3699cd1d517c0
allem40306 <allem40306@gmail.com>
2023-05-26 08:31:14 +0000

    修改：將 table 放到 model.py 中

--- app.py
+++ app.py
@@ -9,40 +9,7 @@ app.config.from_object(config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-class Teacher(db.Model):
-    __tablename__ = "teacher"
-    name = db.Column(db.VARCHAR(30), primary_key = True, nullable = False)
-    email = db.Column(db.VARCHAR(30), nullable = False)
-    office = db.Column(db.VARCHAR(30), nullable = False)
-    course = db.relationship("Course", backref = "teacherInfo")
-
-    def __init__(self, name, email, office):
-        self.name = name
-        self.email = email
-        self.office = office
-
-class Classroom(db.Model):
-    __tablename__ = "classroom"
-    id = db.Column(db.VARCHAR(30), primary_key = True, nullable = False)
-    floor = db.Column(db.INT, nullable = False)
-    capacity = db.Column(db.INT, nullable = False)
-    course = db.relationship("Course", backref = "classroomInfo")
-
-    def __init__(self, id, floor, capacity):
-        self.id = id
-        self.floor = floor
-        self.capacity = capacity
-
-class Course(db.Model):
-    __tablename__ = "course"
-    name = db.Column(db.VARCHAR(30), primary_key = True)
-    teacher = db.Column(db.VARCHAR(30), db.ForeignKey("teacher.name"), nullable = False)
-    roomCode = db.Column(db.VARCHAR(30), db.ForeignKey("classroom.id"), nullable = False)
-
-    def __init__(self, name, teacher, roomCode):
-        self.name = name
-        self.teacher = teacher
-        self.roomCode = roomCode
+from models import *
 
 @app.route("/")
 def hello_world():
